d20c4ad7ad6bcc5250a6109dd12e06d55dc99ce6
hatton-david-byui <114830833+hatton-david-byui@users.noreply.github.com>
2022-10-12 11:58:22 -0400

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,8 +1,7 @@
 board = ["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-",]
-
-currentPlayer = "X"
+player = "X"
 winner = None
 gameRunning = True
 
@@ -20,10 +19,50 @@ printBoard(board)
 def playerInput(board):
     inp = int(input("Enter a number 1 - 9: "))
     if inp >= 1 and inp <= 9 and board[inp-1] == "-":
-        board[inp-1] = currentPlayer
+        board[inp-1] = player
     else:
         print("That location has been played already.")
 #Check for win ore tie
+
+def checkHorizontle(board):
+    global winner
+    if board[0] == board[1] == board[2] and  board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+def checkVertical(board):
+    global winner
+    if board[0] == board[3] == board[6] and  board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def checkDiagnal(board):
+    global winner
+    if board[0] == board[4] == board[8] and  board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[3] != "-":
+        winner = board[2]
+        return True
+
+def checkTie(board):
+    if "-" not in board:
+        printBoard(board)
+        print("Tie Game!")
+        gameRunning = False
+
 
 while gameRunning:
     printBoard(board)
